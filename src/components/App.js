ae27e897a94f205ea02352f88e82677f23ce90f5
Arnold <arnold.hsieh8@gmail.com>
2022-04-22 19:34:52 -0400

    added buy button/quant adjuster

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,32 @@
 import "../styles/App.css";
 import "@fontsource/cabin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Shop from "./Shop";
 import Cart from "./Cart";
 
 function App() {
+    const [cart, setCart] = useState([]); //a cart is an array of objects: {itemID, quantity, price?}
+
+    function addToCart(id, quantity, price) {
+        const oldCart = cart;
+        oldCart.push({ id, quantity, price });
+        setCart(oldCart);
+    }
+
     return (
         <BrowserRouter>
             <div className="App">
                 <NavBar />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/shop" element={<Shop />} />
+                    <Route
+                        path="/shop"
+                        element={<Shop />}
+                        addToCart={addToCart}
+                    />
                     <Route path="/cart" element={<Cart />} />
                 </Routes>
             </div>
